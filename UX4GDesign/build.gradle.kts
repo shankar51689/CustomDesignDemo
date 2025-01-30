c07e8049d684887c3186f4f9a094b92f4522521b
Monu <monu.sharma@netoyed.com>
2025-01-30 11:28:40 +0530

    gradle changes

--- UX4GDesign/build.gradle.kts
+++ UX4GDesign/build.gradle.kts
@@ -34,60 +34,21 @@ android {
 }
 
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            groupId = "com.example.mylibrary"
-            artifactId = "mylibrary"
-            version = "release"
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.github.shankar51689"
+                artifactId = "CustomDesignDemo"
+                version = "1.0.0"
 
-            // Add artifacts
-//            artifact(tasks.getByName("sourcesJar"))
-            artifact("${buildDir}/outputs/aar/mylibrary-release.aar")
-            /*artifact("$buildDir/outputs/aar/mylibrary-release.aar") {
-                builtBy (tasks.named("assembleRelease"))
-            }*/
-            // Configure POM details
-            /*pom {
-                name.set("My Library")
-                description.set("A sample Android library.")
-                url.set("https://github.com/shankar51689/testProj")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-            }*/
-            /*jar {
-                archiveBaseName.set("my-library")  // Name of the jar file
-                archiveVersion.set("1.0.0")        // Version of the jar file
-                from( sourceSets.main.output )       // Specify which files should be included in the jar
-            }*/
+                // Explicitly include the Android library component
+                from(components.findByName("release") ?: return@create)
+            }
         }
     }
-
-    repositories {
-        /*maven {
-//            name = "myMavenRepo"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = "monu40128@gmail.com"
-                password = "Monu@5168900"
-            }
-        }*/
-
-        /*maven { // https://repo1.maven.org/maven2/
-            url = uri("https://trial8y03rm.jfrog.io/artifactory/api/maven/libs-release-local") // Replace with your repo key
-            credentials {
-//                username = (project.findProperty("artifactory_user") ?: System.getenv("ARTIFACTORY_USER")).toString()
-//                password = (project.findProperty("artifactory_password") ?: System.getenv("ARTIFACTORY_PASSWORD")).toString()
-                username = ""
-                password = ""
-            }
-        }*/
-    }
 }
+
 
 dependencies {
 
